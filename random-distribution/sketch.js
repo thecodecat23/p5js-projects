f1829fa04c87dc635d8f8b50fb62d15bb9757cd2
TheCodeCat <thecodecat@gmail.com>
2024-09-19 21:42:34 +0200

    add text for frequenxy and index

--- random-distribution/sketch.js
+++ random-distribution/sketch.js
@@ -2,11 +2,15 @@ let randomCounts = [];
 let total = 20;
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(800, 600);
 
   for (let i = 0; i < total; i++) {
     randomCounts[i] = 0;
   }
+
+  // Set text properties
+  textAlign(CENTER, BOTTOM);
+  textSize(6);
 }
 
 function draw() {
@@ -15,11 +19,26 @@ function draw() {
   let index = floor(random(randomCounts.length));
   randomCounts[index]++;
 
-  stroke(0);
-  fill(26, 242, 83);
   let w = width / randomCounts.length;
 
   for (let x = 0; x < randomCounts.length; x++) {
-    rect(x * w, height - randomCounts[x], (w - 1), randomCounts[x]);
+    let barHeight = randomCounts[x];
+
+    // Set fill color for the bars
+    fill(26, 242, 83);
+    stroke(0);
+
+    // Draw the bar
+    rect(x * w, height - barHeight, w - 1, barHeight);
+
+    // Set text color
+    fill(0); // Black color for text
+    noStroke();
+
+    // Display the count number above each bar
+    text(randomCounts[x], x * w + w / 2, height - barHeight - 2);
+
+    // Display the index number below each bar
+    text(x, x * w + w / 2, height);
   }
 }
