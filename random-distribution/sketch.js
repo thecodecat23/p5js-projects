dde98e78e66fdcf90bd2e89db26e6f3d2957244f
TheCodeCat <thecodecat@gmail.com>
2024-09-19 20:58:01 +0200

    add first example project

---
+++ random-distribution/sketch.js
@@ -0,0 +1,25 @@
+let randomCounts = [];
+let total = 20;
+
+function setup() {
+  createCanvas(200, 200);
+
+  for (let i = 0; i < total; i++) {
+    randomCounts[i] = 0;
+  }
+}
+
+function draw() {
+  background(255);
+
+  let index = floor(random(randomCounts.length));
+  randomCounts[index]++;
+
+  stroke(0);
+  fill(26, 242, 83);
+  let w = width / randomCounts.length;
+
+  for (let x = 0; x < randomCounts.length; x++) {
+    rect(x * w, height - randomCounts[x], (w - 1), randomCounts[x]);
+  }
+}
