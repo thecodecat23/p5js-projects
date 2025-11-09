f7316a66b0b3b13d7a40e85892b8a6b8ec3fdc10
thecodecat23 <thecodecat23@gmail.com>
2025-11-09 22:07:54 +0100

    add multiple walkers

--- weighted-walker-mouse/sketch.js
+++ weighted-walker-mouse/sketch.js
@@ -1,11 +1,31 @@
+let walkers = [];
+
 function setup() {
   createCanvas(400, 400);
-  rColor = color(floor(random(255)), floor(random(255)), floor(random(255)));
-  weightedWalker = new MouseWalker(rColor);
   background(255);
+  addWalker();
 }
 
 function draw() {
-  weightedWalker.step();
-  weightedWalker.show();
+  let r = random(1)
+
+  if (r < 0.1) {
+    addWalker();
+  }
+
+  for (let i = 0; i < walkers.length; i++) {
+    walkers[i].step();
+    walkers[i].show();
+  }
+}
+
+function addWalker() {
+  let rColor = color(floor(random(255)), floor(random(255)), floor(random(255)));
+  let walker = new MouseWalker(rColor);
+  
+  walkers.push(walker);
+
+  if (walkers.length > 50) {
+    walkers.shift();
+  }
 }
